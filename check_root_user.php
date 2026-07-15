d980f2abe5b2d96d6b213e18ecff4a78df74f81b
Zeenux <zeenux@gmail.com>
2026-07-15 13:05:01 +0500

    diagnostics: fix autoloader path

--- check_root_user.php
+++ check_root_user.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/bootstrap/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
